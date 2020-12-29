f70d0f4459d354ea41b573d0d495e6989814a766
brisberg <brandon.risberg@gmail.com>
2020-12-28 22:16:04 -0800

    Added chapel/simple-inventory.js and a footer to display inventory. Doesn't update right away

--- src/js/clue.js
+++ src/js/clue.js
@@ -1,7 +1,5 @@
 $(document).on(':passagerender', (ev) => {
-  console.log(':passagerender ' + ev.passage.title);
   if (ev.passage.tags.includes("clue")) {
-    console.log("Displayed clue passage: " + ev.passage.title);
     const title = ev.passage.title;
     const clueID = title.split(' ')[0];
 
@@ -15,5 +13,3 @@ $(document).on(':passagerender', (ev) => {
     variables().cluesFound = clues
   }
 });
-
-console.log('clue.js loaded')
